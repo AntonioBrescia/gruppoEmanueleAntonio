38fead0127b8bccf146782b56c5a9a25e252ae66
AntonioBrescia <antoniobrescia@rocketmail.com>
2025-11-18 16:25:06 +0100

    add methods and attributes
    aggiunta metodi e attributi alla classe padre

--- Es_matrici/main.py
+++ Es_matrici/main.py
@@ -1,1 +1,20 @@
 # main
+# classe padre 
+class GestoreMatrice:
+    def __init__(self, matrice):
+
+        self.matrice = matrice
+
+    def stampa_matrice(self):
+        for riga in self.matrice:
+            print(riga)
+        print()  
+        
+    def valida_matrice(self):
+        if len(self.matrice) == 0:
+            return True
+        lunghezza = len(self.matrice[0])
+        for riga in self.matrice:
+            if len(riga) != lunghezza:
+                return False
+        return True
